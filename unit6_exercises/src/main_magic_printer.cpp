26438e171ab628c2985dddf1da4a307569afeba2
ramcharanthota <icherryme.0407@gmail.com>
2022-09-15 11:56:00 +0000

    template function implemented

---
+++ unit6_exercises/src/main_magic_printer.cpp
@@ -0,0 +1,16 @@
+#include "unit6_exercises/magic_printer.h"
+
+int main(int argc, char **argv) {
+
+  ros::init(argc, argv, "magic_print_main_node");
+
+  ros::NodeHandle _n("magic_print_ns");
+
+  MagicPrinter magic_printer_object(_n);
+
+  int value_integer = 42;
+  magic_printer_object.PrintGeneric(value_integer);
+  magic_printer_object.PrintGeneric("adventure time");
+
+  return 0;
+}
